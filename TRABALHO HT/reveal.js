93840aa86758ba5d63cad316ad9979a9607b4007
Igor Cardoso Barradas <113136488+RoguiConor@users.noreply.github.com>
2024-09-05 11:43:35 -0300

    Add files via upload

---
+++ TRABALHO HT/reveal.js
@@ -0,0 +1,108 @@
+window.revelar = ScrollReveal({reset:true})
+
+// TOPO
+
+revelar.reveal('.efeito-txt-top', {
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-ing-top', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+// titulo
+
+revelar.reveal('.título', {
+    duration: 2000,
+    distance: '90px'
+})
+
+//Especialidades
+
+revelar.reveal('.efeito-especialidade1', {
+    duration: 2000,
+    distance: '90px',
+    delay:500
+})
+
+revelar.reveal('.efeito-especialidade2', {
+    duration: 2000,
+    distance: '90px',
+    delay:1000
+})
+
+revelar.reveal('.efeito-especialidade3', {
+    duration: 2000,
+    distance: '90px',
+    delay:1500
+})
+
+// Sobre
+
+revelar.reveal('.efeito-img-sobre', {
+    duration:1000,
+    distance: '90px',
+    delay:500,
+    origin: 'left'
+})
+
+revelar.reveal('.efeito-txt-sobre', {
+    duration: 1000,
+    distance: '90px',
+    delay:500,
+    origin: 'right'
+})
+
+//Box
+
+
+revelar.reveal('.efeito-Box1', {
+    duration: 2000,
+    distance: '90px',
+    delay:500
+})
+
+revelar.reveal('.efeito-Box2', {
+    duration: 2000,
+    distance: '90px',
+    delay:1000
+})
+
+revelar.reveal('.efeito-Box3', {
+    duration: 2000,
+    distance: '90px',
+    delay:1500
+})
+
+//Input
+
+revelar.reveal('.efeito-formulario', {
+    duration: 2000,
+    distance: '90px',
+    delay:500
+})
+
+revelar.reveal('.efeito-input1', {
+    duration: 2000,
+    distance: '90px',
+    delay:500
+})
+
+revelar.reveal('.efeito-input2', {
+    duration: 2000,
+    distance: '90px',
+    delay:1000
+})
+
+revelar.reveal('.efeito-input3', {
+    duration: 2000,
+    distance: '90px',
+    delay:1500
+})
+
+
+
+
